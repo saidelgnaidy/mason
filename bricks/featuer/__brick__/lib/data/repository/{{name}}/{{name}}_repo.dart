41c6d77b7d9244eedf0895b89d0f2f2bb6943645
Said Ahmed <97171705+saidelgnaidy@users.noreply.github.com>
2022-08-21 12:38:12 +0200

    .....

---
+++ bricks/featuer/__brick__/lib/data/repository/{{name}}/{{name}}_repo.dart
@@ -0,0 +1,27 @@
+import 'package:{{pro_name}}/data/models/{{name}}/{{name}}_model.dart';
+import 'package:{{pro_name}}/shared/api_client/api_clien_helper.dart';
+import 'package:{{pro_name}}/shared/api_client/endpoints.dart';
+import 'package:{{pro_name}}/shared/error/failuers.dart';
+import 'package:{{pro_name}}/di.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+
+
+abstract class {{name.pascalCase()}}RepoAbs {
+  Future<Either<KFailure, {{name.pascalCase()}}Model>> get_{{name}}();
+}
+
+class {{name.pascalCase()}}RepoImp implements {{name.pascalCase()}}RepoAbs {
+  
+  @override
+  Future<Either<KFailure, SalesDelegateModel>> get_{{name}}() async {
+    Future<Response<dynamic>> func = Di.dioClient.get(KEndPoinst.{{name}}get);
+    final result = await ApiClientHelper.responseToModel(func: func);
+    return result.fold(
+      (l) => left(l),
+      (r) => right({{name.pascalCase()}}Model.fromJson(r)),
+    );
+  }
+
+}
